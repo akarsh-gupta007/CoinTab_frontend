c8a4699b07e5f48955371c4ef9d4c5cdafbee811
Akarshgu <akarshgupta705415@gmail.com>
2023-01-27 13:22:07 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Mainroutes from './Pages/Mainroutes';
+
+function App() {
+  return (
+   <div className='App_container'>
+    <Mainroutes />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UserDetailsTAble.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import UserTable from './UserTable'
+import "./Style.css"
+
+
+
+export default function UserDetailsTAble({ page, setPage, data, number }) {
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>AVTAR</TableCell>
+                            <TableCell align="right">NAME</TableCell>
+                            <TableCell align="right">LOCATION</TableCell>
+                            <TableCell align="right">GENDER</TableCell>
+                            <TableCell align="right">MAIL</TableCell>
+                            <TableCell align="right">PHONE</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data.map(elem => (
+                                <UserTable data={elem} />
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <div className='datachangebtn' >
+                <div className='datachangebtn2'>
+                    <button className='prevandnextbtn' disabled={page == 1} onClick={() => setPage(page - 1)}>PREV</button>
+                    <button className='pagenumberbtn'>{page}</button>
+
+                    <button className='prevandnextbtn' disabled={page == number} onClick={() => { setPage(page + 1) }}>NEXT</button>
+                </div>
+            </div>
+
+
+        </>
+    );
+}
